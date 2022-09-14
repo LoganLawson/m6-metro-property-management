6081805310f54a7e83b0790a4c8ecdd9920a5801
Logan Lawson <l.p.lawson@icloud.com>
2022-09-15 11:40:13 +1200

    build property query backend

---
+++ property-backend/server.js
@@ -0,0 +1,33 @@
+const { MongoClient } = require("mongodb");
+const express = require("express");
+const { response } = require('express');
+const app = express()
+const port = 3005
+
+const uri = "mongodb://root:example@localhost:27017";
+const options = { authSource: "admin" };
+const client = new MongoClient(uri, options);
+
+const retrieve = async (q) => {
+  try {
+    await client.connect(); 
+    const db = client.db("metroProperty");
+    const col = db.collection("properties");
+    const cursor = col.find(q);
+    return await cursor.toArray()
+  } finally {
+    client.close;
+  }
+}
+
+const router = express.Router();
+app.use('/properties', router);
+
+router.get('/', async (req, res) => {
+  const properties = await retrieve(req.query);
+  res.send(properties)
+  })
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
