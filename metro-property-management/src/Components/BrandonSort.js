03276bee6af2e5321813cd2efa572a2d1027f67a
Brandon Trump <brandontrump101@gmail.com>
2022-09-15 14:17:51 +1200

    data showing on frontend

--- metro-property-management/src/Components/BrandonSort.js
+++ metro-property-management/src/Components/BrandonSort.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 function BrandonSort() {
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3005/properties").then((response) => {
+      setProperties(response.data);
+    });
+  }, []);
+
   return (
     <div>
-        <h2>Quick Sort Brandon</h2>
-        <Link to='/Home'> <h6>home</h6> </Link>
+      <h2>Quick Sort Brandon</h2>
+      <Link to="/Home">
+        <h6>home</h6>
+      </Link>
+
+      {properties.map((val) => {
+        return (
+          <div key={val.id}>
+            <p>{val.street}</p>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default BrandonSort
+export default BrandonSort;
--- property-backend/server.js
+++ property-backend/server.js
@@ -1,8 +1,9 @@
 const { MongoClient } = require("mongodb");
 const express = require("express");
-const { response } = require('express');
-const app = express()
-const port = 3005
+const { response } = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3005;
 
 const uri = "mongodb://root:example@localhost:27017";
 const options = { authSource: "admin" };
@@ -10,24 +11,26 @@ const client = new MongoClient(uri, options);
 
 const retrieve = async (q) => {
   try {
-    await client.connect(); 
+    await client.connect();
     const db = client.db("metroProperty");
     const col = db.collection("properties");
     const cursor = col.find(q);
-    return await cursor.toArray()
+    return await cursor.toArray();
   } finally {
     client.close;
   }
-}
+};
+
+app.use(cors());
 
 const router = express.Router();
-app.use('/properties', router);
+app.use("/properties", router);
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   const properties = await retrieve(req.query);
-  res.send(properties)
-  })
+  res.send(properties);
+});
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
+  console.log(`listening on port ${port}`);
+});
