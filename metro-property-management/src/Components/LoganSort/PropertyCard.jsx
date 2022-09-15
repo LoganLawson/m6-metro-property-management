65f64e7ca1e706a93de438eb5159b79e6befe6dd
Logan Lawson <l.p.lawson@icloud.com>
2022-09-16 10:45:47 +1200

    built property card component

---
+++ metro-property-management/src/Components/LoganSort/PropertyCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './PropertyCard.module.css'
+import bedGlyph from '../../images/bed.svg'
+import bathGlyph from '../../images/bath.svg'
+import carGlyph from '../../images/car.svg'
+
+function PropertyCard(props) {
+  console.log(props.propertyData.rate)
+
+  return (
+    <div className={styles.container}>
+      <div className={styles['image-section']}>
+        <img src={props.imageSource} alt='image of property'/>
+      </div>
+      <div className={styles['info-section']}>
+        <span className={styles.title}>{props.propertyData.title}</span>
+        <span className={styles.rate}>${props.propertyData.rate} per week</span>
+        <div className={styles['info-banner']}>
+          <img src={bedGlyph} alt="bed glyph" />{props.propertyData.bedrooms}
+          <img src={bathGlyph} alt="bath glyph" />{props.propertyData.bathrooms}
+          <img src={carGlyph} alt="car glyph" />1
+        </div>
+      </div>
+        
+    </div>
+  )
+};
+
+export default PropertyCard
