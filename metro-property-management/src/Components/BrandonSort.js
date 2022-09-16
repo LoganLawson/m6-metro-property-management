453b6fc0be6947742bdde4a91ce564ec34ee8286
Brandon Trump <brandontrump101@gmail.com>
2022-09-16 14:02:39 +1200

    console logging sorted street names

--- metro-property-management/src/Components/BrandonSort.js
+++ metro-property-management/src/Components/BrandonSort.js
@@ -11,13 +11,28 @@ function BrandonSort() {
     });
   }, []);
 
+  const map = properties.map((x) => x.street);
+
+  const quickSort = (arr) => {
+    if (arr.length < 2) {
+      return arr;
+    }
+    let pivot = arr[arr.length - 1];
+    let left = [];
+    let right = [];
+    for (let i = 0; i < arr.length - 1; i++) {
+      arr[i] < pivot ? left.push(arr[i]) : right.push(arr[i]);
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)];
+  };
+
   return (
     <div>
       <h2>Quick Sort Brandon</h2>
       <Link to="/Home">
         <h6>home</h6>
       </Link>
-
+      <button onClick={() => console.log(quickSort(map))}>Sort Names</button>
       {properties.map((val) => {
         return (
           <div key={val.id}>
