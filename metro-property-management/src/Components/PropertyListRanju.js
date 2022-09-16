d44129cf9d270d95305d380eb4288f77c230f29d
ranjurave <ranjurave@gmail.com>
2022-09-16 17:52:27 +1200

    quick sort wip

--- metro-property-management/src/Components/PropertyListRanju.js
+++ metro-property-management/src/Components/PropertyListRanju.js
@@ -3,7 +3,7 @@ import Navbar from './Navbar'
 import '../Styles/PropertyList.css'
 function PropertyListRanju() {
   const [properties, setProperty] = useState(null);
-  const [propertySortedList, setPropertySortedList] = useState(null);
+  const [propertySortedList, setPropertySortedList] = useState([]);
 
   useEffect(()=>{
     const fetchProperty = async()=>{
@@ -17,15 +17,42 @@ function PropertyListRanju() {
     }
     fetchProperty()
   },[])
-  
-  function quickSort(){
-    setPropertySortedList(properties);
-    console.log("sort called");
-    console.log(propertySortedList);
-    return;
+
+  function updateList(){
+    console.log(properties);
+  }
+  function tocall(){
+    console.log("the sorted List");
+    setPropertySortedList(quickSort(properties));
+    console.log(quickSort(properties));
+  }
+  function quickSort(listToSort){
+    let sortedlist = [];
+    const length = listToSort.length;
+    let start = 1;
+    const pivot = 0;
+    if(listToSort.length<2){
+      setPropertySortedList(listToSort);
+      return;
+    }
+    const left = [];
+    const right = [];
+    while(start <= length-1){
+      if( listToSort[start].rate < listToSort[pivot].rate){
+        left.push(listToSort[start])
+      }
+      else{
+        right.push(listToSort[start])
+      }
+      start++;
+    }
+    let templeft = [...quickSort(left)];
+    let tempright = [...quickSort(right)];
+    sortedlist = [templeft, pivot, tempright];
+    return (sortedlist);
   }
 
-  //Quick sort function
+  //Quicksort function
   // function quickSort(numArray,length = numArray.length,start=1){
   //   if(numArray.length < 2){
   //       return numArray; // base case
@@ -41,18 +68,19 @@ function PropertyListRanju() {
   //     else {
   //       right.push(numArray[start])
   //     }
-  //   start++ //  incrementing start value
+  //     start++ //  incrementing start value
   //   }
-  //   return [...quickSort(left), pivot, ...quickSort(right)]
+  //   let sorted = [...quickSort(left), pivot, ...quickSort(right)];
+  //   return sorted;
   // }
-  // console.log(quickSort([9,8,7,6,1,4,3,2,5]));
-  //console.log(properties);
+  // console.log(quickSort([9,3,7,6,1,4,8,2,5]));
 
   return (
     <div className='property-list'>
       <Navbar/>
       <h1>Property List</h1>
-      <button onClick={quickSort}>Click Sort</button>
+      <button onClick={updateList}>Click Update</button>
+      <button onClick={tocall}>Click Sort</button>
       <table>
         <thead>
         <tr>
@@ -66,13 +94,13 @@ function PropertyListRanju() {
         </thead>
         <tbody>
         {properties && properties.map((property) => (
-        <tr className="border-top">
-          <td> <p key = {property._id}> {property.title} </p> </td>
-          <td> <p key = {property._id}>  {property.rate} </p> </td>
-          <td> <p key = {property._id}>  {property.bedrooms} </p> </td>
-          <td> <p key = {property._id}>  {property.bathrooms} </p> </td>
-          <td> <p key = {property._id}>  {property.city} </p> </td>
-          <td> <p key = {property._id}>  {property.pets} </p> </td>
+        <tr className="border-top" key = {property._id}>
+          <td> <p> {property.title} </p> </td>
+          <td> <p> {property.rate} </p> </td>
+          <td> <p> {property.bedrooms} </p> </td>
+          <td> <p> {property.bathrooms} </p> </td>
+          <td> <p> {property.city} </p> </td>
+          <td> <p> {property.pets} </p> </td>
         </tr>
         ))}
         </tbody>
