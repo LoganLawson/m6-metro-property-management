9b71c9e76d274033ac33b846ff035efeab367a05
Brandon Trump <brandontrump101@gmail.com>
2022-09-18 02:11:30 +1200

    side arrows added to hero card

--- metro-property-management/src/Components/HeroSection/HeroCard.jsx
+++ metro-property-management/src/Components/HeroSection/HeroCard.jsx
@@ -1,10 +1,18 @@
 import styles from "./HeroSection.module.css";
+import sideArrow from "../../images/side-arrow.svg";
 
 const HeroCard = ({ title, text }) => {
   return (
     <div className={styles.heroCards}>
       <h2 className={styles.cardTitle}>{title}</h2>
-      <p className={styles.cardText}>{text}</p>
+      <p className={styles.cardText}>
+        {text}
+        <img
+          src={sideArrow}
+          alt="nav down arrow"
+          className={styles.cardTextArrow}
+        />
+      </p>
     </div>
   );
 };
