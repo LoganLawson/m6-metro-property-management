7e9b6c9b0175cd979dcbdc16ca48cce19efc9913
ranjurave <ranjurave@gmail.com>
2022-09-19 10:45:21 +1200

    sorting catogories added

--- metro-property-management/src/Components/PropertyListRanju.js
+++ metro-property-management/src/Components/PropertyListRanju.js
@@ -2,8 +2,8 @@ import React, {useEffect, useState} from 'react'
 import Navbar from './Navbar'
 import '../Styles/PropertyList.css'
 function PropertyListRanju() {
-  const [properties, setProperty] = useState(null);
   const [propertySortedList, setPropertySortedList] = useState(null);
+  const [properties, setProperty] = useState(propertySortedList);
 
   useEffect(()=>{
     const fetchProperty = async()=>{
@@ -20,12 +20,32 @@ function PropertyListRanju() {
     fetchProperty()
   },[])
 
-  function ApplySort(){
-    const listSorted = quickSort(properties);
-    setPropertySortedList(listSorted);
+  function ApplySort(category)
+  {
+    let listSorted = [];
+    switch(category){
+      case "rates":
+      listSorted = quickSortRates(properties);
+      setPropertySortedList(listSorted);
+      break;
+    case "bedrooms":
+      listSorted = quickSortBedrooms(properties);
+      setPropertySortedList(listSorted);
+      break;
+    case "bathrooms":
+      listSorted = quickSortBathrooms(properties);
+      setPropertySortedList(listSorted);
+      break;
+    case "pets":
+      listSorted = quickSortPets(properties);
+      setPropertySortedList(listSorted);
+      break;
+    default:
+      break;
+    }
   }
 
-  function quickSort(listToSort){
+  function quickSortRates(listToSort){
     if(listToSort.length < 2){
       return listToSort;
     }
@@ -34,7 +54,7 @@ function PropertyListRanju() {
     let right = [];
     let index = 0;
     while(index < listToSort.length-1){
-      if( listToSort[index].rate > pivot.rate){
+      if( listToSort[index].rate < pivot.rate){
         left.push(listToSort[index])
       }
       else{
@@ -42,7 +62,70 @@ function PropertyListRanju() {
       }
       index++;
     }
-    const sortedlist = [...quickSort(left), pivot, ...quickSort(right)];
+    const sortedlist = [...quickSortRates(left), pivot, ...quickSortRates(right)];
+    return (sortedlist);
+  }
+
+  function quickSortBedrooms(listToSort){
+    if(listToSort.length < 2){
+      return listToSort;
+    }
+    const pivot = listToSort[listToSort.length - 1];
+    let left = [];
+    let right = [];
+    let index = 0;
+    while(index < listToSort.length-1){
+      if( listToSort[index].bedrooms > pivot.bedrooms){
+        left.push(listToSort[index])
+      }
+      else{
+        right.push(listToSort[index])
+      }
+      index++;
+    }
+    const sortedlist = [...quickSortBedrooms(left), pivot, ...quickSortBedrooms(right)];
+    return (sortedlist);
+  }
+
+  function quickSortBathrooms(listToSort){
+    if(listToSort.length < 2){
+      return listToSort;
+    }
+    const pivot = listToSort[listToSort.length - 1];
+    let left = [];
+    let right = [];
+    let index = 0;
+    while(index < listToSort.length-1){
+      if( listToSort[index].bathrooms > pivot.bathrooms){
+        left.push(listToSort[index])
+      }
+      else{
+        right.push(listToSort[index])
+      }
+      index++;
+    }
+    const sortedlist = [...quickSortBedrooms(left), pivot, ...quickSortBedrooms(right)];
+    return (sortedlist);
+  }
+
+  function quickSortPets(listToSort){
+    if(listToSort.length < 2){
+      return listToSort;
+    }
+    const pivot = listToSort[listToSort.length - 1];
+    let left = [];
+    let right = [];
+    let index = 0;
+    while(index < listToSort.length-1){
+      if( listToSort[index].pets > pivot.pets){
+        left.push(listToSort[index])
+      }
+      else{
+        right.push(listToSort[index])
+      }
+      index++;
+    }
+    const sortedlist = [...quickSortBedrooms(left), pivot, ...quickSortBedrooms(right)];
     return (sortedlist);
   }
 
@@ -50,16 +133,16 @@ function PropertyListRanju() {
     <div className='property-list'>
       <Navbar/>
       <h1>Property List</h1>
-      <button onClick={ApplySort}>Click Sort</button>
+      <button onClick={() => ApplySort('rates')}>Load Properties</button>
       <table>
         <thead>
         <tr>
           <th className="border-top">Address</th>
-          <th>Rate</th>
-          <th>Bedrooms</th>
-          <th>Bathrooms</th>
-          <th>City</th>
-          <th>Pets</th>
+          <th><button onClick={() => ApplySort('rates')}>Rates</button></th>
+          <th><button onClick={() => ApplySort('bedrooms')}>Bedrooms</button></th>
+          <th><button onClick={() => ApplySort('bathrooms')}>Bathrooms</button></th>
+          <th><button onClick={() => ApplySort('city')}>City</button></th>
+          <th><button onClick={() => ApplySort('pets')}>Pets</button></th>
         </tr>
         </thead>
         <tbody>
