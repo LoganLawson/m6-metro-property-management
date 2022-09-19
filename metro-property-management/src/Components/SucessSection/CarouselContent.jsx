76eef8c37ad19e59b932540b3f07ed970e70e601
Brandon Trump <brandontrump101@gmail.com>
2022-09-20 10:01:47 +1200

    updated carousel

--- metro-property-management/src/Components/SucessSection/CarouselContent.jsx
+++ metro-property-management/src/Components/SucessSection/CarouselContent.jsx
@@ -1,20 +1,14 @@
 import styles from "./SucessSection.module.css";
-import nextArrow from "../../images/carousel/carousel-next.svg";
-import prevArrow from "../../images/carousel/carousel-prev.svg";
 
 const CarouselContent = ({ slideImg, title, name, text }) => {
   return (
     <div className={styles.slideContainer}>
-      <img src={prevArrow} alt="previous" className={styles.slideButton} />
-
       <img src={slideImg} alt="slide" />
       <div className={styles.slideContent}>
         <h1 className={styles.slideTitle}>{title}</h1>
         <h3 className={styles.slideName}>{name}</h3>
         <p className={styles.slideText}>{text}</p>
       </div>
-
-      <img src={nextArrow} alt="next" className={styles.slideButton} />
     </div>
   );
 };
