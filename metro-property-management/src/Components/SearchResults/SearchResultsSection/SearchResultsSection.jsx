d5b20d1144891db7ad0337c11e80cad305ba5ee0
Logan Lawson <l.p.lawson@icloud.com>
2022-09-21 22:48:42 +1200

    create filter dropdown

--- metro-property-management/src/Components/SearchResults/SearchResultsSection/SearchResultsSection.jsx
+++ metro-property-management/src/Components/SearchResults/SearchResultsSection/SearchResultsSection.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import ReactPaginate from 'react-paginate';
+import React, { useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 import styles from './SearchResultsSection.module.css'
+
+import Dropdown from 'react-dropdown';
+import downCaret from '../../../images/down-caret-dark.svg'
 
 import PropertyCard from '../../PropertyCard/PropertyCard'
 import quickSortArray from '../quickSortArray.js'
@@ -27,6 +28,16 @@ function SearchResultsSection(props) {
     </div>
   );
 
+  const sortOptions = [
+    'Featured', 'Rate', 'Bedrooms', 'Bathrooms', 'Carparks'
+  ];
+
+  function handleChange(e) {
+    // set sort state here
+    console.log(e.value)
+  }
+
+
   console.log('current items', currentItems)
   console.log('all items', items)
 
@@ -35,7 +46,12 @@ function SearchResultsSection(props) {
       <div className={styles['results-1']}>
         <span id={styles['section-heading']}> We found {items.length} results for you</span>
         <div className={styles['result-options']}>
-          <div>Sort by:</div><div>Show map</div>
+          <div className={styles['filter-dropdown']}> 
+            <label>Sort by:</label>
+            <Dropdown menuClassName={styles.dropdown} options={sortOptions} onChange={handleChange} value={sortOptions[0]} />
+            <img src={downCaret} alt="" />
+          </div>
+          <div>Show map</div>
         </div>
         <div className={styles['property-cards']}>{propertyCards}</div>
       </div>
