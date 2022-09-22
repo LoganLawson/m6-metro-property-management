0c856149dfefaf02ce2a2fadb53f445bca63da49
ranjurave <ranjurave@gmail.com>
2022-09-23 02:02:33 +1200

    search window almost

--- metro-property-management/src/Components/PopupWindows/DropDown.jsx
+++ metro-property-management/src/Components/PopupWindows/DropDown.jsx
@@ -1,47 +1,48 @@
-import React, { useEffect, useState } from 'react';
-// type DropDownProps = {
-//     cities: string[];
-//     showDropDown: boolean;
-//     toggleDropDown: Function;
-//     citySelection: Function;
-//     };
-    function DropDown(props) {
-        const [showDropDown, setShowDropDown] = useState(false);
-
-        /**
-         * Handle passing the city name
-         * back to the parent component
-         *
-         * @param city  The selected city
-         */
-        const onClickHandler = (city) => {
-            props.citySelection(city);
-        };
+    import React, { useEffect, useState } from 'react';
+    import './PropertySearchPopup.css'
+    // type DropDownProps = {
+    //     cities: string[];
+    //     showDropDown: boolean;
+    //     toggleDropDown: Function;
+    //     citySelection: Function;
+    //     };
+        function DropDown(props) {
+            const [showDropDown, setShowDropDown] = useState(false);
     
-        useEffect(() => {
-        setShowDropDown(showDropDown);
-        }, [showDropDown]);
+            /**
+             * Handle passing the city name
+             * back to the parent component
+             *
+             * @param item  The selected city
+             */
+            const onClickHandler = (item) => {
+                props.itemSelection(item);
+            };
+        
+            useEffect(() => {
+            setShowDropDown(showDropDown);
+            }, [showDropDown]);
+        
+            return (
+            <>
+                <div className={showDropDown ? 'dropdown' : 'dropdown active'}>
+                {props.items.map(
+                    (item, index)=> {
+                    return (
+                        <p
+                        key={index}
+                        onClick={() => {
+                            onClickHandler(item);
+                        }}
+                        >
+                        {item}
+                        </p>
+                    );
+                    }
+                )}
+                </div>
+            </>
+            );
+        }
     
-        return (
-        <>
-            <div className={showDropDown ? 'dropdown' : 'dropdown active'}>
-            {props.cities.map(
-                (city, index)=> {
-                return (
-                    <p
-                    key={index}
-                    onClick={() => {
-                        onClickHandler(city);
-                    }}
-                    >
-                    {city}
-                    </p>
-                );
-                }
-            )}
-            </div>
-        </>
-        );
-    }
-
-export default DropDown
+    export default DropDown
